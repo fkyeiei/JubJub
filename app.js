419443e990d852df72736f6c69f4a5d24c4a85a3
fkyeiei <frank29112005@gmail.com>
2024-07-19 10:26:43 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-document.getAnimationsById("myForm").addEventlistener("submit", function(event){
-    event.preventDeFault();
+document.getElementById("myform").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    let formdata = new formdata(this);
+    let formdata = new FormData(this);
     let result = "Name : "+
     formdata.get("name")+ "<br>" + "email :" +
     formdata.get("email")+ "<br>" + "age :" +
     formdata.get("age")+ "<br>" + "gender :" +
     formdata.get("gender")+ "<br>" + "commants :" +
     formdata.get("commants");
-    document.getElementById("result").innerHTML = "<h1> my data </h1> + result";
+    document.getElementById("result").innerHTML = "<h1> my data </h1>" + result;
 })
