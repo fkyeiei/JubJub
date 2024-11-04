9c1db8cc3675edd5c7c0c65460d53685127aaf45
fkyeiei <frank29112005@gmail.com>
2024-11-04 10:14:24 +0700

    Add files via upload

---
+++ งานเทอม 2/app.js
@@ -0,0 +1,21 @@
+// let number1 = 'test'
+// let number2 = 'num'
+// let number3 = number1 + number2
+// console.log('new number is', number3)    
+
+
+// let number1 = 5
+// let number2 = 5
+// let condition1 = number1 >= number2
+// Boolean(true ,false)
+// console.log('result of condition is',condition1)
+
+if - else condition
+if (number1 != number2){
+    console.log('this is if')}
+
+else if(number1 == number2){
+    console.log('this is else if')}
+
+else{console.log1('this is else')}
+
