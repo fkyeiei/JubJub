13198b8f3bd01279801af0ac0580782127c138a1
fkyeiei <frank29112005@gmail.com>
2024-11-07 13:28:17 +0700

    Add files via upload

---
+++ fky/app.js
@@ -0,0 +1,25 @@
+prompt ("ให้ user ใส่อะไร")
+let score = prompt('put you score')
+if (score >80) {
+    console.console.log('เกรดที่ได้ ; A')
+}
+else if (score >70) {
+    console.console.log('เกรดที่ได้ ; B')
+}
+else if (score >60) {
+    console.console.log('เกรดที่ได้ ; C')
+}
+else if (score >50) {
+    console.console.log('เกรดที่ได้ ; D')
+}
+else if {
+    console.console.log('เกรดที่ได้ ; F')
+}
+//}
+// else {
+//    console.log('เกรดที่ได้ : F')
+//}
+
+// let text ='exit'
+//while(text !='exit'){
+//     text = rompt('ใส่ข้อความมาใหม่')}
