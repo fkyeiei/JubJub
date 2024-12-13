362fd55d4d90c22e6e7411b18f362bee1f93d3e4
fkyeiei <frank29112005@gmail.com>
2024-12-13 12:33:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const exp = require ('express')
 const app = exp()
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.json()) // มาแบบ text
 
 const port = 8000 
 
+
+// path = /
 app.get('/',(req,res)=>{
 let user = {
     firsname : 'หมา',
@@ -10,8 +15,18 @@ let user = {
     age : 50,
     gender : 'male',
 }
+res.json(user)
 
+})
 
+app.port('/user',(req,res)=>{
+    let user = req.body
+
+    users.push(user)
+
+    res.json({
+        message : 'add complete'
+    })
 })
 
 app.listen(port,(req,res)=>{
